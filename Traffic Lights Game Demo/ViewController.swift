a0b09659bee9fc9852396d8b2a5a1c544cdf06cb
Mirko Cukich <mirkocukich@gmail.com>
2019-07-03 23:18:13 -0500

    Final Code for Traffic Lights Game Demo App for iPhone devices

--- Traffic Lights Game Demo/ViewController.swift
+++ Traffic Lights Game Demo/ViewController.swift
@@ -33,15 +33,73 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Game score starts at zero
+        scoreInt = 0
+        
     }
     
     // Start Game Button
     @IBAction func startGameBtn(_ sender: Any) {
         
-        
+        // start game play ready to play
+        if scoreInt == 0 {
+          // top graphic starts at blank, then down colors from Red to Yellow to Green
+            timerInt = 3
+          // used to display image
+            trafficLightImage.image = UIImage(named: "trafficLight")
+          // trigger countdown, timer repeats every second
+            startTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+            
+            // disables start button
+            startStopBtn.isEnabled = false
+            // sets the button text
+            startStopBtn.setTitle("", for: UIControl.State.normal)
+            // when restarting the game it changes the label text
+            textLbl.text = String(scoreInt)
+            
+        } else {
+            // if you press btn when game is running it will invalidate the score
+            scoreTimer.invalidate()
+            // score int = to zero in the backgroun,
+            scoreInt = 0
+            // changes button text to Restart
+            startStopBtn.setTitle("Restart", for: UIControl.State.normal)
+        }
     }
     
-
+    // timer used to change the traffic light graphic at the top
+    @objc func updateTimer(){
+        // takes 1 away from timerInt until it gets to 0
+        timerInt -= 1
+        
+        if timerInt == 2 {
+            // Red color
+            trafficLightImage.image = UIImage(named: "trafficLight3")
+            
+        } else if timerInt == 1 {
+            // Yellow color
+            trafficLightImage.image = UIImage(named: "trafficLight2")
+            
+        } else if timerInt == 0 {
+            // Green color
+            trafficLightImage.image = UIImage(named: "trafficLight1")
+            // stops timer and invalidates
+            startTimer.invalidate()
+            // time clock
+            scoreTimer = Timer.scheduledTimer(timeInterval: 0.0001, target: self, selector: #selector(updateScoreTimer), userInfo: nil, repeats: true)
+            
+            // enables start button
+            startStopBtn.isEnabled = true
+            // sets the button text
+            startStopBtn.setTitle("Stop", for: UIControl.State.normal)
+        }
+    }
+    // updates the score time to start increasing
+    @objc func updateScoreTimer(){
+        // updates the scoreInt
+        scoreInt += 1
+        textLbl.text = String(scoreInt)
+        
+    }
 }
 
